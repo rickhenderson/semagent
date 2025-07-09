c95a3076102772b30717492a7d8dcfbc9ef7652f
Lost Diffusion <tassit@gmail.com>
2025-07-09 12:13:21 -0400

    Get something running after paying OpenAI.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿// Import packages
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+// From https://learn.microsoft.com/en-us/semantic-kernel/get-started/quick-start-guide?pivots=programming-language-csharp
+
+// Populate values from your OpenAI deployment
+var modelId = "gpt-4o-mini";
+var endpoint = "https://192.168.2.21:77";
+var apiKey = "sucker";
+
+// Create a kernel with Azure OpenAI chat completion
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+// Add enterprise components
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+// Build the kernel
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Add a plugin (the LightsPlugin class is defined below)
+kernel.Plugins.AddFromType<LightsPlugin>("Lights");
+
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+// Initiate a back-and-forth chat
+string? userInput;
+do
+{
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    // Add user input
+    if (!string.IsNullOrEmpty(userInput))
+        history.AddUserMessage(userInput);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
--- README.md
+++ README.md
@@ -6,7 +6,21 @@ A few days agao I was contacted by a business owner who is starting an AI agent-
 
 I thought this was yet another reason to finally get into actually working with AI Agents.
 
-I use a lot of pre-made AI and have been following genAI for a while. I use Claude.ai fairly regularly for basic knowledge checks, have tried no-code platforms for app development, interact frequeintly with conversational AI, and have spent a lot of time dealing with Stable-Diffusion type image generation with Flux and ComfyUI.
+I use a lot of pre-made AI and have been following genAI for a while. I use [Claude.ai](https://claude.ai/) fairly regularly for basic knowledge checks, have tried no-code platforms for app development, interact frequeintly with conversational AI, and have spent a lot of time dealing with Stable-Diffusion type image generation with Flux and ComfyUI.
 
-I was attending a Tines presentation (it's actually still going on as I type this) and decided to take a look at a few frameworks to see where I can start.
+I was attending a [Tines](https://www.tines.com/) presentation (it's actually still going on as I type this) and decided to take a look at a few frameworks to see where I can start.
+
+About Semantic Kernel: [https://learn.microsoft.com/en-us/semantic-kernel/overview/]
+
+While you can start with existing examples by cloning the Semantic Kernel repo, I wanted to start from scratch.
+
+I chose C# because I have been doing too much Python lately.
+
+Of course, using any kind of model that doesn't run locally requires paid API calls, so I did put $10 into OpenAI API, but then I'll also have to set up a working webserver to handle the API requests.
+
+But so far, it's a good starting point.
+
+# Links and References
+
+* [Human-In-The-Loop Example](https://learn.microsoft.com/en-us/semantic-kernel/frameworks/process/examples/example-human-in-loop?pivots=programming-language-csharp)
 
